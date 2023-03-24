84ba5fa29ccb63f79d058ab3f1a87dc46dfdca39
DevInformation <44543774+DevInfomation@users.noreply.github.com>
2023-03-24 14:58:29 +0100

    Add files via upload

---
+++ thomas-frank-website/js/main-page-script.js
@@ -0,0 +1,11 @@
+const whiteBtn = document.getElementById('white-btn');
+const purpleBtn = document.getElementById('purple-btn');
+
+whiteBtn.addEventListener('click', () => {
+    open('https://thomasjfrank.com/templates/');
+});
+
+purpleBtn.addEventListener('click', () => {
+    open('https://thomasjfrank.com/fundamentals/');
+});
+
